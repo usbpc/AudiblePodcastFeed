c71edad6dbfa93b089e9674c5fa5c0a23ddde683
Kevin Holm <kevin@holm.dev>
2025-11-02 00:26:51 +0100

    Added HTTP Basic auth
    Link: #8

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+import base64
+import binascii
+import logging
 import os
 import hashlib
 import random
@@ -7,8 +10,11 @@ from typing import Any
 from datetime import datetime, timedelta
 from email.utils import format_datetime
 
+from starlette.authentication import AuthenticationBackend, AuthenticationError, AuthCredentials, SimpleUser
 from starlette.exceptions import HTTPException
 from starlette.applications import Starlette
+from starlette.middleware import Middleware
+from starlette.middleware.authentication import AuthenticationMiddleware
 from starlette.requests import Request
 from starlette.routing import Route, Mount
 from starlette.staticfiles import StaticFiles
@@ -21,9 +27,36 @@ config = Config(".env")
 
 PODCAST_FEED_IMAGE = config.get("PODCAST_FEED_IMAGE")
 HASH_SALT = bytes(config.get("PODCAST_HASH_SALT", default=''.join(random.choices(string.ascii_letters + string.digits, k=16))), 'utf-8')
+AUTH_ENABLED = config.get("AUTH_ENABLED", default=True)
+HTTP_USER = config.get("HTTP_USERNAME", default="user")
+
+try:
+    HTTP_PASSWORD = config.get("HTTP_PASSWORD")
+except KeyError as exc:
+    HTTP_PASSWORD = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
+    logging.warning(f"No HTTP_PASSWORD set, the password will be randomly generated on each start! Current password: {HTTP_PASSWORD}. ")
 
 folder_settings.AUDIO_FOLDER = config.get("AUDIO_FOLDER", default="audio_files")
 folder_settings.METADATA_FOLDER = config.get("METADATA_FOLDER", default="metadata_files")
+
+class BasicAuthBackend(AuthenticationBackend):
+    async def authenticate(self, conn):
+        if "Authorization" not in conn.headers:
+            return
+
+        auth = conn.headers["Authorization"]
+        try:
+            scheme, credentials = auth.split()
+            if scheme.lower() != 'basic':
+                return
+            decoded = base64.b64decode(credentials).decode("ascii")
+        except (ValueError, UnicodeDecodeError, binascii.Error) as exc:
+            raise AuthenticationError('Invalid basic auth credentials')
+
+        username, _, password = decoded.partition(":")
+        if username != HTTP_USER or password != HTTP_PASSWORD:
+            return
+        return AuthCredentials(["authenticated"]), SimpleUser(username)
 
 from folder_settings import AUDIO_FOLDER
 from book_store import get_all_individual_books, get_series_by_asin, get_podcast_by_asin
@@ -87,8 +120,13 @@ def generate_url_prefix(request: Request):
     url_prefix = f'{scheme}://{host}:{port}' if port else f'{scheme}://{host}'
     return url_prefix
 
+def auth_check(request: Request):
+    if AUTH_ENABLED and not request.user.is_authenticated:
+        raise HTTPException(status_code=401, headers={'WWW-Authenticate': 'Basic realm="audiobook podcasts"'})
+
 @add_route(path='/individual_books')
 def individual_books(request: Request):
+    auth_check(request)
     books = get_all_individual_books()
 
     url_prefix = generate_url_prefix(request)
@@ -109,7 +147,7 @@ def individual_books(request: Request):
         })
 
     data = {
-        'title': 'Kevin\'s Audiobooks',
+        'title': 'Audiobooks not in any series',
         'description': 'Audiobooks provided as a Podcast Feed for use in Podcast Apps',
         'image_url': PODCAST_FEED_IMAGE,
         'items': items
@@ -118,6 +156,7 @@ def individual_books(request: Request):
 
 @add_route(path='/podcast/{asin}')
 def podcast_series(request: Request):
+    auth_check(request)
     asin = request.path_params['asin']
     podcast = get_podcast_by_asin(asin)
 
@@ -152,6 +191,7 @@ def podcast_series(request: Request):
 
 @add_route(path='/series/{asin}')
 def book_series(request: Request):
+    auth_check(request)
     asin = request.path_params['asin']
     series = get_series_by_asin(asin)
 
@@ -186,4 +226,8 @@ def book_series(request: Request):
     return templates.TemplateResponse(request, 'podcast.xml.j2', data, media_type='text/xml')
 
 routes.append(Mount('/audio_file', app=SaltHashStaticfiles(directory=AUDIO_FOLDER), name='audio_files'))
-app = Starlette(debug=True, routes=routes)
+
+middleware = [
+    Middleware(AuthenticationMiddleware, backend=BasicAuthBackend())
+]
+app = Starlette(debug=True, routes=routes, middleware=middleware)
